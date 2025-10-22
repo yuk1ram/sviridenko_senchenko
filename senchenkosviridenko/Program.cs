6f8eac42f9f87270079049638be36fc3b9cbeef2
Ваша Фамилия <your.email@example.com>
2025-10-22 16:07:11 +0500

    project

---
+++ senchenkosviridenko/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace senchenkosviridenko
+{
+
+class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Сумма: {num1 + num2}");
+            Console.WriteLine($"Разность: {num1 - num2}");
+            Console.WriteLine($"Произведение: {num1 * num2}");
+            Console.WriteLine($"Частное: {num1 / num2}");
+        }
+    }
+}
+
